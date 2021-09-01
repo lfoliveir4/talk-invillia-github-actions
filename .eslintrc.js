a3a8e63872cec5146b4ef71b69bbd3a45c0fc5a0
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2021-09-01 00:49:05 -0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,27 @@
 module.exports = {
-  root:true,
   env: {
-    jest: true,
+    commonjs: true,
+    node: true,
     browser: true,
+    es6: true,
+    jest: true,
   },
-}
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {},
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "react-hooks"],
+  ignorePatterns: ["node_modules/"],
+  rules: {},
+  settings: {
+    react: {
+      version: "latest", // "detect" automatically picks the version you have installed.
+    },
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 
 export default function App(): ReactElement {
   return (
