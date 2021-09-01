1215bb210180d709116c5512de9b1a4487c32230
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2021-09-01 00:10:01 -0300

    test deploy on external branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,8 @@ export default function App(): ReactElement {
         alignItems: 'center'
       }}
     >
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
-      <p>Invilli</p>
+      <p>Invillia</p>
+      <strong>A invillia é uma empresa excelente</strong>
     </div>
   )
 }
